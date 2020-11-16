6c03b2a895418f17ac1fbfa8dba1afb9ce1366d3
monalisali <monalisali@hotmail.com>
2020-11-16 18:15:41 +0800

    update keyword

--- src/main/java/com/github/monalisali/zhihu/getkeywords/Crawler.java
+++ src/main/java/com/github/monalisali/zhihu/getkeywords/Crawler.java
@@ -12,7 +12,7 @@ public class Crawler {
 
     public static void main(String[] args) throws IOException {
         System.out.println("**************************开始*********************");
-        Search search = new Search("冲牙器");
+        Search search = new Search("保温壶");
         System.out.println("关键词参数:" + search.getSearchParam());
         List<String> hotWords = search.getDropDownListHotwords();
         WriteFile writeFile = new WriteFile(search.getSearchParam());
