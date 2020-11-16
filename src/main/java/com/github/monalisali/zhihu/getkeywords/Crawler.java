438bb5798818f024840c3020fec849c445ab8b12
monalisali <monalisali@hotmail.com>
2020-11-16 20:24:23 +0800

    直接从控制台输入关键字

--- src/main/java/com/github/monalisali/zhihu/getkeywords/Crawler.java
+++ src/main/java/com/github/monalisali/zhihu/getkeywords/Crawler.java
@@ -1,9 +1,6 @@
 package com.github.monalisali.zhihu.getkeywords;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +9,10 @@ public class Crawler {
 
     public static void main(String[] args) throws IOException {
         System.out.println("**************************开始*********************");
-        Search search = new Search("保温壶");
+        System.out.println("请输入关键字，按回车键结束");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String keyword = br.readLine();
+        Search search = new Search(keyword);
         System.out.println("关键词参数:" + search.getSearchParam());
         List<String> hotWords = search.getDropDownListHotwords();
         WriteFile writeFile = new WriteFile(search.getSearchParam());
